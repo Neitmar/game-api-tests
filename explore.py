04b6f1c68347fbc6b02f2483862f9e4ddc3b96dc
Neitmar <Neitmar@gmail.com>
2026-09-02 21:52:09 +0300

    test: migrate genre check to pytest

--- explore.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get("https://www.freetogame.com/api/games?category=shooter")
-games = response.json()
-
-for game in games:
-    assert game["genre"] == "Shooter", f"Пришла игра не того жанра: {game['title']}"
